d74e6494f4f159c88c494e5a45f4b6412dccb9e3
lucasmodin <lumo0002@stud.kea.dk>
2024-11-20 12:52:07 +0100

    initial commit

---
+++ src/main/java/alignpro/AlignProRealApplication.java
@@ -0,0 +1,13 @@
+package alignpro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlignProRealApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlignProRealApplication.class, args);
+    }
+
+}
---
+++ src/test/java/alignpro/AlignProRealApplicationTests.java
@@ -0,0 +1,13 @@
+package alignpro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlignProRealApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
