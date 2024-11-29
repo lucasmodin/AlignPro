7c3b49245e55b7982d4b3d585e85bd6df525d9da
lucasmodin <lumo0002@stud.kea.dk>
2024-11-29 21:31:42 +0100

    commit for oskar

--- src/main/java/alignpro/Controller/ProjectOverview/AlignProController.java
+++ src/main/java/alignpro/Controller/ProjectOverview/AlignProController.java
@@ -58,83 +58,74 @@ public class AlignProController {
     //TODO fix endpoint when user/login session is implemented
     @GetMapping("/pm-dashboard/{pmUserID}")
     public String getDashboard(@PathVariable("pmUserID") int pmUserID, Model model){
-        // Initialize the list to hold the final DTOs
         List<ProjectDTO> dashboardData = new ArrayList<>();
 
-        // Fetch projects for the user
+
         List<Project> projects = alignProService.getAllProjects(pmUserID);
 
         for (Project project : projects) {
-            // Create and populate a ProjectDTO
+
             ProjectDTO projectDTO = new ProjectDTO();
             projectDTO.setProjectName(project.getProjectName());
             projectDTO.setStartDate(project.getStartDate());
             projectDTO.setDeadline(project.getDeadLine());
             projectDTO.setTotalSumTime(project.getTotalTime());
 
-            // Fetch subprojects for the current project
+
             List<SubProject> subProjects = alignProService.getAllSubProjects(project.getProjectID());
             List<SubProjectDTO> subProjectDTOs = new ArrayList<>();
 
             for (SubProject subProject : subProjects) {
-                // Create and populate a SubProjectDTO
+
                 SubProjectDTO subProjectDTO = new SubProjectDTO();
                 subProjectDTO.setSubProjectName(subProject.getSubProjectName());
                 subProjectDTO.setStartDate(subProject.getStartDate());
                 subProjectDTO.setEndDate(subProject.getEndDate());
 
-                // Fetch tasks for the current subproject
+
                 List<Task> tasks = alignProService.getAllTasks(subProject.getSubProjectID());
                 List<TaskDTO> taskDTOs = new ArrayList<>();
 
                 for (Task task : tasks) {
-                    // Create and populate a TaskDTO
+
                     TaskDTO taskDTO = new TaskDTO();
                     taskDTO.setTaskName(task.getTaskName());
                     taskDTO.setStartDate(task.getStartDateString());
                     taskDTO.setEndDate(task.getEndDateString());
 
-                    // Fetch subtasks for the current task
                     List<SubTask> subTasks = alignProService.getAllSubTasks(task.getTaskID());
                     List<SubTaskDTO> subTaskDTOs = new ArrayList<>();
 
                     for (SubTask subTask : subTasks) {
-                        // Create and populate a SubTaskDTO
+
                         SubTaskDTO subTaskDTO = new SubTaskDTO();
                         subTaskDTO.setSubTaskName(subTask.getSubTaskName());
                         subTaskDTO.setStartDate(subTask.getStartDate());
                         subTaskDTO.setEndDate(subTask.getEndDate());
                         subTaskDTO.setEstimatedTime(subTask.getTime());
 
-                        // Add the SubTaskDTO to the list
+
                         subTaskDTOs.add(subTaskDTO);
                     }
 
-                    // Add subtasks to the TaskDTO
+
                     taskDTO.setSubtasks(subTaskDTOs);
 
-                    // Add the TaskDTO to the task list
                     taskDTOs.add(taskDTO);
                 }
 
-                // Add tasks to the SubProjectDTO
                 subProjectDTO.setTasks(taskDTOs);
 
-                // Add the SubProjectDTO to the subproject list
                 subProjectDTOs.add(subProjectDTO);
             }
 
-            // Add subprojects to the ProjectDTO
             projectDTO.setSubProjects(subProjectDTOs);
 
-            // Add the ProjectDTO to the dashboard data
             dashboardData.add(projectDTO);
         }
 
-        // Add the dashboard data to the model
         model.addAttribute("dashboardData", dashboardData);
 
-        // Return the view name
         return "pm-Dashboard";
     }
 
