d35d3e0f838869effe6291541be698237b4e4763
OSBJE <osbj0001rstud.kea.dk>
2024-12-02 19:55:05 +0100

    filter logic added to DashBoard_DTO class

--- src/main/java/alignpro/Model/DTOModel/DashBoard_DTO.java
+++ src/main/java/alignpro/Model/DTOModel/DashBoard_DTO.java
@@ -1,8 +1,11 @@
 package alignpro.Model.DTOModel;
 
 import alignpro.Model.Projects.Project;
+import alignpro.Model.Projects.SubProject;
 import alignpro.Model.Projects.SubTask;
+import alignpro.Model.Projects.Task;
 
+import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
@@ -50,14 +53,67 @@ public class DashBoard_DTO {
         this.subTaskList = subTaskList;
     }
 
+    //**** Method to return a list to pick how to sort ****//
+
+    public List<String> filterList(){
+        List<String> filterList = new ArrayList<>();
+
+        for (ProjectDTO project : projectList){
+            filterList.add(project.getProjectName());
+        }
+
+        return filterList;
+    }
+
+
     //***** methods to sort itself *****//
 
-    public void filterList(String input) {
+    public void filter(String input){
+        filterProjects(input);
+        filterSubProjects(input);
+        filterTask(input);
+        filterSubTask(input);
+    }
+
+    public void filterProjects(String input) {
         Iterator<ProjectDTO> iterator = projectList.iterator();
 
         while (iterator.hasNext()) {
             ProjectDTO item = iterator.next();
             if (!item.getProjectName().contains(input)) {
+                iterator.remove(); // Removes items that do not contain the input
+            }
+        }
+    }
+
+    public void filterSubProjects(String input){
+        Iterator<SubProjectDTO> iterator = subProjectList.iterator();
+
+        while (iterator.hasNext()) {
+            SubProjectDTO item = iterator.next();
+            if (!item.getFilter().contains(input)) {
+                iterator.remove(); // Removes items that do not contain the input
+            }
+        }
+    }
+
+    public void filterTask(String input){
+        Iterator<TaskDTO> iterator = taskList.iterator();
+
+        while (iterator.hasNext()) {
+            TaskDTO item = iterator.next();
+            if (!item.getFilter().contains(input)) {
+                iterator.remove(); // Removes items that do not contain the input
+            }
+        }
+    }
+
+    public void filterSubTask(String input){
+        Iterator<SubTaskDTO> iterator = subTaskList.iterator();
+
+        while (iterator.hasNext()) {
+            SubTaskDTO item = iterator.next();
+            if (!item.getFilter().contains(input)) {
                 iterator.remove(); // Removes items that do not contain the input
             }
         }
