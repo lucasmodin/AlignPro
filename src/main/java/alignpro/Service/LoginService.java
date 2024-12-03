c4682aec8ea66755cdf150c9615d620ab39431aa
lucasmodin <lumo0002@stud.kea.dk>
2024-12-03 11:32:38 +0100

    login done

--- src/main/java/alignpro/Service/LoginService.java
+++ src/main/java/alignpro/Service/LoginService.java
@@ -1,5 +1,6 @@
 package alignpro.Service;
 
+import alignpro.Model.ProjectManager;
 import alignpro.Repository.Interfaces.ILoginController;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
@@ -12,5 +13,18 @@ public class LoginService {
 
     public LoginService(ApplicationContext context, @Value("${login.repository.impl}") String impl) {
         this.loginController = (ILoginController) context.getBean(impl);
+    }
+
+    public ProjectManager getProjectManager(String mail) {
+        return loginController.getProjectManager(mail);
+    }
+
+
+
+    public boolean loginCheck(String mail, String password) {
+        ProjectManager projectManager = getProjectManager(mail);
+        if (projectManager.getMail().equals(mail) && projectManager.getPassword().equals(password)) {
+            return true;
+        } else return false;
     }
 }
